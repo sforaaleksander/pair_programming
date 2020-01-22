b2bd3f94a716fb44dc5377561226b3691ffc178d
Alek <sforaaleksander@gmail.com>
2020-01-22 13:08:51 +0100

    added years function

--- years/years_module.py
+++ years/years_module.py
@@ -2,11 +2,15 @@ import datetime
 
 
 def years(age):
-    return
+    year_now = datetime.datetime.today().year
+    user_is_100 = 100 - age + year_now
+    return user_is_100
 
 
 def main():
-    return
+    user_name = input("Type in your name: ")
+    user_age = int(input("Type in you age: "))
+    print(f"{user_name}, you will be 100 year old in {years(user_age)}")
 
 
 if __name__ == '__main__':
